5cfc79ecca4f1a2a02dbab19bf8001f395a87f5c
Walter Capa <walterc316@gmail.com>
2022-03-09 16:23:56 -0500

    Now the login form dissapears if authenticated

--- src/components/Layout/Menu.js
+++ src/components/Layout/Menu.js
@@ -2,9 +2,11 @@
 import styles from './Menu.module.css';
 import { Col, Container, Form, InputGroup, Nav, Navbar, NavDropdown, Row, Button } from 'react-bootstrap';
 import LoginForm from '../Forms/Login';
+import { useSelector } from 'react-redux';
 
 
 const Menu = props => {
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
   return (
     <Navbar bg="warning" variant="dark" expand="lg">
       <Container>
@@ -16,9 +18,11 @@ const Menu = props => {
             <Nav.Link href="">Accounts</Nav.Link>
             <Nav.Link href="">Transactions</Nav.Link>
           </Nav>
-          <Nav className="ms-auto">
-            <LoginForm />
-          </Nav>
+          {!isAuthenticated &&
+            <Nav className="ms-auto">
+              <LoginForm />
+            </Nav>
+          }
         </Navbar.Collapse>
       </Container>
     </Navbar>
