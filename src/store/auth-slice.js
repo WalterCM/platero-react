41964fc19c35b1eb42d8cf055f92425a41a8628a
Walter Capa <walterc316@gmail.com>
2022-03-20 00:56:08 -0500

    Now login is preserved

--- src/store/auth-slice.js
+++ src/store/auth-slice.js
@@ -3,18 +3,25 @@ import { createSlice } from '@reduxjs/toolkit';
 const authSlice = createSlice({
   name: 'auth',
   initialState: {
-    isAuthenticated: false,
-    access: '',
-    refresh: ''
+    isAuthenticated: !!(localStorage.getItem('access')),
+    access: localStorage.getItem('access'),
+    refresh: localStorage.getItem('refresh')
   },
   reducers: {
     login(state, action) {
       state.isAuthenticated = true
       state.access = action.payload.access;
       state.refresh = action.payload.refresh;
+      localStorage.setItem('access', action.payload.access);
+      localStorage.setItem('refresh', action.payload.refresh);
     },
     logout(state) {
       state.isAuthenticated = false
+
+      state.access = null;
+      state.refresh = null;
+      localStorage.removeItem('access');
+      localStorage.removeItem('refresh');
     },
   }
 });
